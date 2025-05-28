eb230d2a4586cc82e8801c39d85a384de9a78b75
Caio Orleans <caioorleansdm@gmail.com>
2025-05-28 12:47:33 -0300

    Initial commit

---
+++ src/main/java/com/github/caioorleans/familytodo/FamilyToDoApplication.java
@@ -0,0 +1,13 @@
+package com.github.caioorleans.familytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyToDoApplication.class, args);
+    }
+
+}
